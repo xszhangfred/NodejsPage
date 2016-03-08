eed46bd9c7845e5f622a6c30b562d6741cc16593
xszhangfred <xszhang.fred@gmail.com>
2016-03-08 16:30:56 -0500

    Font Awesome and Error Handler

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -2,21 +2,27 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
 
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
+
+
 gulp.task('sass', function(){
   gulp.src('styles/main.scss')
-      .pipe(sass())
+      .pipe(sass()).on('error', handleError)
       .pipe(prefix())
       .pipe(gulp.dest('../styles'));
 });
 
 gulp.task('cp', function(){
-	gulp.src('index.html')
-		.pipe(gulp.dest('..'));
+  gulp.src('index.html')
+      .pipe(gulp.dest('..'));
 });
 
 gulp.task('watch', function(){
-	gulp.watch(['*.html'], ['cp']);
-	gulp.watch(['styles/*.scss'], ['sass']);
-});
+  gulp.watch(['*.html'], ['cp']);
+  gulp.watch(['styles/*.scss'], ['sass']);
+})
 
 gulp.task('default', ['sass', 'cp', 'watch']);
